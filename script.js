ae2efb41756e7bc515fc136e30fdf2b3fcdd288e
Radomir Wojtera <radomir.wojtera@gmail.com>
2018-03-12 20:45:02 +0100

    genarate data

--- script.js
+++ script.js
@@ -70702,6 +70702,12 @@ const elements = require("./elements");
 const log = require("./log");
 const gen = require("./gen");
 
+let numRows = 20;
+
+elements.genData.addEventListener("click", () => {
+    console.log(gen(numRows).map(row => `"${row.join('","')}"\n`));
+    elements.textarea.value = gen(numRows).map(row => `"${row.join('","')}"\n`).join("");
+})
 
 gen(20).map(log);
 
